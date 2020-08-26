a1461c5656878a22c801ec9c383847ba95e5528f
Jez-from-the-6 <j.schering@web.de>
2020-08-26 19:34:31 +0200

    making use of the submodule structure

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+import './skeleton/src/css/app.css';
+import React from "react"
+import { render } from 'react-dom';
+import App from "./skeleton/app";
+
+render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
 let config = require("./skeleton/webpack.config");
+const path = require('path');
+const entrypath = path.join(__dirname, '/index.tsx')
+const outputpath = path.resolve(__dirname, 'dist')
+const contentBase = path.join(__dirname, 'dist')
 
-module.exports = config()
+module.exports = config(entrypath, outputpath, contentBase)
